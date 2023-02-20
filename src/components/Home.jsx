119a252311b691bdffe720b9e11f98e4b27d8c2a
nishaflyweis <nisharaitani19@gmail.com>
2023-02-20 22:10:12 +0530

    :

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,34 +19,34 @@ const Home = () => {
 
   const navigate = useNavigate()
 
- 
 
-  const initial= [{
+
+  const initial = [{
     firstName: "",
     lastName: "",
     email: "",
     phone: "",
-  
 
-}]
 
-const [credentials, setCredentials] = useState(initial)
-const [disable, setDisable] = useState(true)
-  const[hide, setHide] = useState('none')
-const setButton = () => {
-  if(hide === 'none'){
-  setHide('flex')
+  }]
+
+  const [credentials, setCredentials] = useState(initial)
+  const [disable, setDisable] = useState(true)
+  const [hide, setHide] = useState('none')
+  const setButton = () => {
+    if (hide === 'none') {
+      setHide('flex')
+    }
+    else {
+      setHide('none')
+    }
   }
-  else{
-    setHide('none')
-  }
-}
 
   const onChange = (e) => {
-    setCredentials({...credentials, [e.target.name]:e.target.value})
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
     // setDisable(false)
     console.log("happy", credentials.phone)
-    if((credentials.phone.length > 8) && (credentials.firstName.length> 0)){
+    if ((credentials.phone.length > 8) && (credentials.firstName.length > 0)) {
 
       setDisable(false)
     }
@@ -61,56 +61,56 @@ const setButton = () => {
   return (
     <>
       <div className="all" id='appointment'>
-        
+
         <div className="mainpic_right">
-        <div className="forme">
-<p  className='text-center m-1 text-white fw-bolder mt-1 ' style={{fontSize: '32px'}}>Book An</p>
-<p  className='text-center m-1 text-white fw-bolder mt-1 mb-4' style={{fontSize: '32px'}}>Appointment Today</p>
-<form  onSubmit={letcheck}>
-  {/* <!-- 2 column grid layout with text inputs for the first and last names --> */}
+          <div className="forme">
+            <p className='text-center m-1 text-white fw-bolder mt-1 ' style={{ fontSize: '32px' }}>Book An</p>
+            <p className='text-center m-1 text-white fw-bolder mt-1 mb-4' style={{ fontSize: '32px' }}>Appointment Today</p>
+            <form onSubmit={letcheck}>
+              {/* <!-- 2 column grid layout with text inputs for the first and last names --> */}
 
-  <div className="row mb-4">
-    <div className="col">
-      <div className="form-outline">
-        <input type="text" id="form3Example1" name='firstName' className="form-control" onChange={onChange} />
-        <label className="form-label" htmlFor="form3Example1">First name</label>
-      </div>
-    </div>
-    <div className="col">
-      <div className="form-outline">
-        <input type="text" id="form3Example2" name='lastName' className="form-control" onChange={onChange} />
-        <label className="form-label" htmlFor="form3Example2">Last name</label>
-      </div>
-    </div>
-  </div>
+              <div className="row mb-4">
+                <div className="col">
+                  <div className="form-outline">
+                    <input type="text" id="form3Example1" name='firstName' className="form-control" onChange={onChange} />
+                    <label className="form-label" htmlFor="form3Example1">First name</label>
+                  </div>
+                </div>
+                <div className="col">
+                  <div className="form-outline">
+                    <input type="text" id="form3Example2" name='lastName' className="form-control" onChange={onChange} />
+                    <label className="form-label" htmlFor="form3Example2">Last name</label>
+                  </div>
+                </div>
+              </div>
 
-  {/* <!-- Email input --> */}
-  <div className="form-outline mb-4">
-    <input type="email" id="form3Example3" name='email' className="form-control" onChange={onChange} />
-    <label className="form-label" htmlFor="form3Example3">Email address</label>
-  </div>
+              {/* <!-- Email input --> */}
+              <div className="form-outline mb-4">
+                <input type="email" id="form3Example3" name='email' className="form-control" onChange={onChange} />
+                <label className="form-label" htmlFor="form3Example3">Email address</label>
+              </div>
 
-  {/* <!-- Password input --> */}
-  <div className="form-outline mb-4">
-    <input type="text" id="form3Example4" name='phone' className="form-control" onChange={onChange} />
-    <label className="form-label" htmlFor="form3Example4">Phone Number</label>
-  </div>
+              {/* <!-- Password input --> */}
+              <div className="form-outline mb-4">
+                <input type="text" id="form3Example4" name='phone' className="form-control" onChange={onChange} />
+                <label className="form-label" htmlFor="form3Example4">Phone Number</label>
+              </div>
 
-  {/* <!-- Checkbox --> */}
-  <div className="form-check d-flex justify-content-center mb-4">
-    <input className="form-check-input me-2" style={{width: '50px', height: '20px'}} type="checkbox" value="flex" id="form2Example33" default={'checked'} name='isSaw' onClick={setButton}/>
-    <label className="form-check-label" htmlFor="form2Example33" style={{fontSize: '13px', fontWeight: '10px'}}>
-    I declare that the information provided above is correct. I would like to receive communication about Dental Care services.
-    </label>
-  </div>
+              {/* <!-- Checkbox --> */}
+              <div className="form-check d-flex justify-content-center mb-4">
+                <input className="form-check-input me-2" style={{ width: '50px', height: '20px' }} type="checkbox" value="flex" id="form2Example33" default={'checked'} name='isSaw' onClick={setButton} />
+                <label className="form-check-label" htmlFor="form2Example33" style={{ fontSize: '13px', fontWeight: '10px' }}>
+                  I declare that the information provided above is correct. I would like to receive communication about Dental Care services.
+                </label>
+              </div>
 
-  {/* <!-- Submit button --> */}
-  <div className="d-flex justify-content-center">
-  <button  type="submit" disabled={disable} style={{display : `${hide}`}}  className="btn btn-light btn-block mb-4">SUBMIT</button>
-  </div>
+              {/* <!-- Submit button --> */}
+              <div className="d-flex justify-content-center">
+                <button type="submit" disabled={disable} style={{ display: `${hide}` }} className="btn btn-light btn-block mb-4">SUBMIT</button>
+              </div>
 
-</form>
-</div>
+            </form>
+          </div>
         </div>
       </div>
 
@@ -120,7 +120,7 @@ const setButton = () => {
       <Cardcarousel />
       <FAQ />
       <Gmap />
-      <Footer/>
+      <Footer />
       {/* <Form/> */}
     </>
 
